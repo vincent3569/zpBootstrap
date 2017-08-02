cb0288d3a5b96775aff2bf08f4aabec6579d257c
vincent3569 <vincent_bourganel@yahoo.fr>
2017-08-03 00:56:10 +0200

    typo

--- album.php
+++ album.php
@@ -1,6 +1,6 @@
 <?php
 /************************************************************************/
-/* redirect to album script, depending on zpB_use_isotope options set  */
+/* redirect to album script, depending on zpB_use_isotope options set   */
 /************************************************************************/
 
 if (getOption('zpB_use_isotope')) {
